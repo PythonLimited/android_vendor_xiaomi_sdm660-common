34a45f56ef940b4213501f093b6e9cfed59e443d
pythonlimited <leonardomeitz@gmail.com>
2019-08-02 10:29:16 +0200

    VNDK: build real vndk

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,49 +1,46 @@
-ifeq ($(BOARD_VNDK_VERSION),)
-$(warning ************* BOARD VNDK is not enabled - compiling vndk-sp ***************************)
 LOCAL_PATH := $(call my-dir)
 
-include $(LOCAL_PATH)/vndk-sp-libs.mk
+EXTRA_VENDOR_LIBRARIES_32 := \
+    libaudioclient \
+    libaudiomanager \
+    libbinder \
+    libbufferhubqueue \
+    libcamera_client \
+    libft2 \
+    libgui \
+    libharfbuzz_ng \
+    libheif \
+    libicui18n \
+    libicuuc \
+    libmedia \
+    libmediaextractor \
+    libmediametrics \
+    libminikin \
+    libpdx_default_transport \
+    libsonivox
 
-vndk_sp_dir := vndk-sp-$(PLATFORM_VNDK_VERSION)
-
-define define-vndk-sp-lib
+define define-vndk-lib
 include $$(CLEAR_VARS)
-LOCAL_MODULE := $1.vndk-sp-gen
+LOCAL_MODULE := $1.vndk-ext-gen
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_PREBUILT_MODULE_FILE := $$(TARGET_OUT_INTERMEDIATE_LIBRARIES)/$1.so
+LOCAL_PREBUILT_MODULE_FILE := $$(call intermediates-dir-for,SHARED_LIBRARIES,$1,,,$3,)/$1.so
 LOCAL_STRIP_MODULE := false
-LOCAL_MULTILIB := first
+LOCAL_MULTILIB := $2
 LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)
+LOCAL_VENDOR_MODULE := true
 include $$(BUILD_PREBUILT)
-
-ifneq ($$(TARGET_2ND_ARCH),)
-ifneq ($$(TARGET_TRANSLATE_2ND_ARCH),true)
-include $$(CLEAR_VARS)
-LOCAL_MODULE := $1.vndk-sp-gen
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_PREBUILT_MODULE_FILE := $$($$(TARGET_2ND_ARCH_VAR_PREFIX)TARGET_OUT_INTERMEDIATE_LIBRARIES)/$1.so
-LOCAL_STRIP_MODULE := false
-LOCAL_MULTILIB := 32
-LOCAL_MODULE_TAGS := optional
-LOCAL_INSTALLED_MODULE_STEM := $1.so
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)
-include $$(BUILD_PREBUILT)
-endif # TARGET_TRANSLATE_2ND_ARCH is not true
-endif # TARGET_2ND_ARCH is not empty
 endef
 
-$(foreach lib,$(VNDK_SP_LIBRARIES),\
-    $(eval $(call define-vndk-sp-lib,$(lib))))
+$(foreach lib,$(EXTRA_VENDOR_LIBRARIES_32),\
+    $(eval $(call define-vndk-lib,$(lib),32,$(TARGET_2ND_ARCH_VAR_PREFIX))))
+
+$(foreach lib,$(EXTRA_VENDOR_LIBRARIES_64),\
+    $(eval $(call define-vndk-lib,$(lib),first,)))
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := vndk-sp
+LOCAL_MODULE := vndk-ext
 LOCAL_MODULE_TAGS := optional
-LOCAL_REQUIRED_MODULES := $(addsuffix .vndk-sp-gen,$(VNDK_SP_LIBRARIES))
+LOCAL_REQUIRED_MODULES := $(addsuffix .vndk-ext-gen,$(EXTRA_VENDOR_LIBRARIES_32) $(EXTRA_VENDOR_LIBRARIES_64))
 include $(BUILD_PHONY_PACKAGE)
-
-vndk_sp_dir :=
-endif
--- wayne.mk
+++ wayne.mk
@@ -417,9 +417,16 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# VNDK-SP:
+# VNDK
 PRODUCT_PACKAGES += \
-    vndk-sp
+	libdng_sdk.vendor_32 \
+	libstdc++.vendor \
+	vndk-ext \
+	vndk_package
+
+PRODUCT_COPY_FILES += \
+	prebuilts/vndk/v27/arm/arch-arm-armv7-a-neon/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.frameworks.sensorservice@1.0-v27.so \
+	prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.frameworks.sensorservice@1.0-v27.so
 
 # Wifi
 PRODUCT_PACKAGES += \
