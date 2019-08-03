65193c89c1238e1cb3953d104f434cb2859fbaf9
pythonlimited <leonardomeitz@gmail.com>
2019-08-03 19:04:22 +0200

    sdm660: import aosp libperfmgr power service

--- sdm660.mk
+++ sdm660.mk
@@ -324,7 +324,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+	android.hardware.power@1.3-service.sdm660-libperfmgr
+
+PRODUCT_COPY_FILES += \
+	$(PLATFORM_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
