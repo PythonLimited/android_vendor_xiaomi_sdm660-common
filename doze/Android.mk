c2372e95bbe1f4d9f1ebce8e1217256a06873552
pythonlimited <leonardomeitz@gmail.com>
2019-08-03 18:43:58 +0200

    sdm660: its 2019

--- doze/Android.mk
+++ doze/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The Xiaomi-SDM660 Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@
 #
 
 LOCAL_PATH:= $(call my-dir)
-ifeq ($(BOARD_VENDOR_PLATFORM),xiaomi-sdm660)
+ifeq ($(TARGET_BOARD_PLATFORM),sdm660)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
